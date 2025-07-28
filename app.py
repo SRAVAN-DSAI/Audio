35095e04cf85f40ee3de68de74910a6ea6bc2f88
SRAVAN KODARI <sravankodari4@gmail.com>
2025-07-28 21:17:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(
     initial_sidebar_state="expanded",
     menu_items={
         'Get Help': 'https://github.com/SRAVAN-DSAI/Audio-Classifier',
-        'Report a Bug': 'sravankodari4@gmail.com',
+        'Report a Bug': 'mailto:sravankodari4@gmail.com',
         'About': 'UrbanSound8K Audio Classifier by Kodari Sravan'
     }
 )
